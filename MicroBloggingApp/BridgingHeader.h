35223f71f1e835023a71dee482862cebb0b5373c
niksmitty <niksmitty@rambler.ru>
2018-03-02 17:26:11 +0500

    All setting files and changed project file.

---
+++ MicroBloggingApp/BridgingHeader.h
@@ -0,0 +1,9 @@
+//
+//  BridgingHeader.h
+//  MicroBloggingApp
+//
+//  Created by Nikita Votyakov on 28.02.18.
+//  Copyright © 2018 Nikita Votyakov. All rights reserved.
+//
+
+#import "UIViewController+Alerts.h"
